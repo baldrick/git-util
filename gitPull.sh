16ee954e4ccda79223b94f37ed0fce7d1a77ec7f
Gavin Manning <gavin.manning@db.com>
2011-02-08 17:00:37 +0000

    Checks for correct usage.  Copes with multiple directories to update.

--- gitPull.sh
+++ gitPull.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
 
-startDirectory=$1
-if [ -d $startDirectory/.git ]
+usage()
+{
+    echo "Usage: $0 <directory>"
+    exit -1
+}
+
+if [ $# -eq 0 ]
 then
-    pushd $startDirectory
-    git pull
-    popd
-else
-    echo pulling $startDirectory
-    for directory in `ls -Al $startDirectory | grep ^d | awk '{printf("%s\n",$9);}'`
-    do
-        ./gitPull.sh $startDirectory/$directory
-    done
+    usage
 fi
+
+while [ $# -gt 0 ]
+do
+
+    startDirectory=$1
+    if [ ! -d $startDirectory ]
+    then
+        echo "Directory $startDirectory does not exist..."
+    else
+        if [ -d $startDirectory/.git ]
+        then
+	    echo "-------------------------------"
+	    echo "-- Updating $startDirectory"
+	    echo "-------------------------------"
+            pushd $startDirectory >/dev/null
+            git pull
+            popd >/dev/null 
+        else
+	    echo "-----------------------------------------------------------------------"
+	    echo "-- Recursively updating $startDirectory"
+	    echo "-----------------------------------------------------------------------"
+            echo pulling $startDirectory
+            for directory in `ls -Al $startDirectory | grep ^d | awk '{printf("%s\n",$9);}'`
+            do
+                ./gitPull.sh $startDirectory/$directory
+            done
+        fi
+    fi
+
+    shift
+
+done
